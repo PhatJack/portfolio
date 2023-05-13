5152036c261c9d84718f9c207e18d24dd2869c9f
PhatJack <tienphatng.693@gmail.com>
2023-05-13 13:58:00 +0700

    init project and add tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./index.css";
+import Footer from "./layouts/Footer";
+import Header from "./layouts/Header";
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
