5ecd2488dce2d0cee00830403c1f9f747fb9dd83
PhatJack <tienphatng.693@gmail.com>
2023-05-28 09:50:57 +0700

    test particle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,106 @@
+import { useCallback } from "react";
 import "./App.css";
 import "./index.css";
 import Footer from "./layouts/Footer";
 import Header from "./layouts/Header";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 function App() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
   return (
     <>
+      <div className="bg-gradient-to-r from-pink-400 to-blue-400 z-20">
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "#000",
+              },
+            },
+            fpsLimit: 90,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: false,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "grab",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+                grab: {
+                  distance: 200,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                directions: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 3,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: 2,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      </div>
       <Header></Header>
       <Footer></Footer>
     </>
