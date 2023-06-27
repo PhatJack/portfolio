a5ba2dd9161c0b476cddb41885f9e414146f70ac
PhatJack <tienphatng.693@gmail.com>
2023-06-27 19:39:17 +0700

    test deploy

--- src/Page/EducationPage.js
+++ src/Page/EducationPage.js
@@ -4,7 +4,6 @@ import React from 'react'
 import Container from '../components/container/Container'
 import CustomSubTitle from '../components/subtitle/CustomSubTitle'
 import { HiOutlineBriefcase } from 'react-icons/hi'
-import { Timeline } from 'flowbite-react'
 function EducationPage() {
 	return (
 		<Container>
